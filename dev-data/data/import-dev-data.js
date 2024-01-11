a4f085a2c25b0f13eede303624c0ff29016d4c2f
Harsh <153886419+Harsh-nodejs@users.noreply.github.com>
2024-01-11 10:12:40 +0530

    w

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -9,22 +9,16 @@ dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
+  process.env.DATABASE_PASSWORD,
 );
 
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  })
-  .then(() => console.log('DB connection successful!'));
+mongoose.connect(DB, {}).then(() => console.log('DB connection successful!'));
 
 // READ JSON FILE
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
 const reviews = JSON.parse(
-  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'),
 );
 
 // IMPORT DATA INTO DB
